6f8deb06ed357f18a3eda402d75a105461dd7b17
khj <bbb6254177@gmail.com>
2022-12-29 18:47:03 +0900

    initial commit

---
+++ src/main/java/khj/eatingrecord/EatingRecordApplication.java
@@ -0,0 +1,13 @@
+package khj.eatingrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatingRecordApplication.class, args);
+    }
+
+}
