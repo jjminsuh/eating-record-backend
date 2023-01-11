0f6feea7b82d136acabf047709523afab4bf0ff3
khj <bbb6254177@gmail.com>
2023-01-11 18:06:21 +0900

    feat(repository): add sql to get member from database

---
+++ src/main/java/khj/eatingrecord/repository/MemberRepository.java
@@ -0,0 +1,29 @@
+package khj.eatingrecord.repository;
+
+import jakarta.persistence.EntityManager;
+import khj.eatingrecord.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    @Autowired
+    public MemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Optional<Member> findById(String id) {
+        Member member =  em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+}
