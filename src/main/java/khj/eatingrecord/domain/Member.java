a5d9e9693b32e1c509923ca80a1231c7938c69bc
khj <bbb6254177@gmail.com>
2023-01-11 18:04:28 +0900

    feat(domain): add member domain

---
+++ src/main/java/khj/eatingrecord/domain/Member.java
@@ -0,0 +1,72 @@
+package khj.eatingrecord.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private String id;
+    private int height;
+    private Character gender;
+    private int age;
+    private int weight;
+    private int num_meal;
+    private String goal;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getNum_meal() {
+        return num_meal;
+    }
+
+    public void setNum_meal(int num_meal) {
+        this.num_meal = num_meal;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+}
