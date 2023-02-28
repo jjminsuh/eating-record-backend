389fd84acc06ee17dd748929ed53c998e0da6277
khj <bbb6254177@gmail.com>
2023-02-28 15:56:08 +0900

    test(sign-up): add test for sign up request

--- src/test/java/khj/eatingrecord/controller/MemberControllerTest.java
+++ src/test/java/khj/eatingrecord/controller/MemberControllerTest.java
@@ -2,19 +2,23 @@ package khj.eatingrecord.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import khj.eatingrecord.dto.MemberDto;
+import khj.eatingrecord.repository.MemberRepository;
 import khj.eatingrecord.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(MemberController.class)
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 class MemberControllerTest {
 
     @Autowired
@@ -23,11 +27,15 @@ class MemberControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+
     @Test
-    void testLogin() throws Exception {
+    void loginTest() throws Exception {
 
         MemberDto memberDto = new MemberDto();
         memberDto.setId("1234abcd");
@@ -43,5 +51,29 @@ class MemberControllerTest {
         String actualJson = result.getResponse().getContentAsString();
 
         assertThat(actualJson).isEqualTo(expectedJson);
+    }
+
+    @Test
+    void signUpTest() throws Exception {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setId("1234abcd");
+        memberDto.setAge(20);
+
+        String expectedJson = "{\"id\":\"1234abcd\",\"height\":0,\"gender\":null,\"age\":20,\"weight\":0,\"num_meal\":0,\"goal\":null}";
+
+        MvcResult result = mockMvc.perform(post("/sign-up")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(memberDto)))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+
+        assertThat(actualJson).isEqualTo(expectedJson);
+
+        MemberDto responseDto = objectMapper.readValue(actualJson, MemberDto.class);
+
+        assertThat(responseDto.getId()).isEqualTo(memberDto.getId());
+        assertThat(responseDto.getAge()).isEqualTo(memberDto.getAge());
     }
 }
